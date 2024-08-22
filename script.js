fc73df8acc6fda1237272abfeb8d8ea4109b6e08
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-22 07:36:37 +0500

    main

---
+++ script.js
@@ -0,0 +1,50 @@
+// let arr = [
+//   {neam: "lorem", id: 4, sum: 10, age: 10},
+//   {neam: "lorem", id: 3, sum: 10, age: 10},
+//   {neam: "lorem", id: 2, sum: 10, age: 10},
+//   {neam: "lorem", id: 1, sum: 10,  age: 10}
+// ]
+
+// const x = (user) => {
+//   let sum = 0;
+
+//   for (let i of users){
+//     sum += i.sum
+//     i.sum = 0
+//   }
+
+//   for (let i = 0; i <= user.length; i++){
+//     if(user[i].id == 1){
+//       user[i].sum = sum;
+//     }
+//   }
+// }
+// x(arr)
+
+// console.log();
+
+
+
+// let count = 0;
+// let number = 7;
+// for(let i = 0; i <= number; i++){
+//   if(number % i == 0){
+//     count++;
+//   }
+// }
+
+// if(count == 2){
+//     console.log("tub");
+// }else{
+//   console.log("tub emas");
+  
+// }
+
+
+// let arr = [                   
+//   {nema: "lorem", id: 1, age: 10},
+//   {nema: "lorem", id: 2, age: 10},
+//   {nema: "lorem", id: 3, age: 10},
+//   {nema: "lorem", id: 4, age: 10}
+// ]
+
